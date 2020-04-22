cf3f989f6b38d78f6b5d383e6f2d85e7dcf30d9b
sidakinaddd <sidakinaddd@gmail.com>
2020-04-22 16:26:07 +0600

    2 fbv for city

---
+++ Krisha/city/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import City
+
+
+class CitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = City
+        fields = ('id', 'name',)
--- Krisha/city/views.py
+++ Krisha/city/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from .models import City
+from .serializers import CitySerializer
+from rest_framework.response import Response
+
 
 # Create your views here.
+@api_view(['GET', 'POST'])
+def city_list(request, format=None):
+    if request.method == 'GET':
+        cities = City.objects.all()
+        serializer = CitySerializer(cities, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = CitySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def city_detail(request, pk, format=None):
+    try:
+        city = City.objects.get(pk=pk)
+    except City.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = CitySerializer(city)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = CitySerializer(city, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        city.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
